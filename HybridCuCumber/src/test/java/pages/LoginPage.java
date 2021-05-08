3f6c7ec2553dbd2e633adfd0fd1ee50901f50376
hp <hp@Kameshwaran>
2021-05-08 22:18:11 +0530

    Hybrid LoginTestcase

--- HybridCuCumber/src/test/java/pages/LoginPage.java
+++ HybridCuCumber/src/test/java/pages/LoginPage.java
@@ -1,5 +1,35 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import baseClass.Library;
+
+public class LoginPage extends Library{
+
+	
+	
+	@FindBy(id="txtUsername")
+	WebElement username;
+	
+	@FindBy(id="txtPassword")
+	WebElement pwd;
+	
+	@FindBy(className="button")
+	WebElement Loginbutton;
+	
+	public  LoginPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		
+		
+}
+	public void login(String uname,String pass) {
+		username.sendKeys(uname);
+		pwd.sendKeys(pass);
+		
+		Loginbutton.click();
+}
 }
--- HybridCuCumber/src/test/java/testCases/LoginTestCase.java
+++ HybridCuCumber/src/test/java/testCases/LoginTestCase.java
@@ -1,5 +1,36 @@
 package testCases;
 
-public class LoginTestCase {
+import java.io.IOException;
 
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import baseClass.Library;
+import pages.LoginPage;
+import utilities.SeleniumUtilities;
+
+public class LoginTestCase extends Library{
+
+	
+	@BeforeTest
+	public void stratUp() {
+		launchApplication("chrome","https://opensource-demo.orangehrmlive.com/");
+		
+	}
+	@Test
+	public void login() {
+		LoginPage lpage=new LoginPage(driver);
+		lpage.login("admin", "admin123");
+		
+		
+	}
+	@AfterTest
+	public void close() throws IOException {
+		SeleniumUtilities util = new SeleniumUtilities(driver);
+		util.to_take_screenshot("src/test/resources/Screenshots/orangehrm.png");
+		quit();
+		
+	}
 }
